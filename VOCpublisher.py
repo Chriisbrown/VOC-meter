ac8638823ada0197328499af5f57780b1cb22fb0
Chriisbrown <41648127+Chriisbrown@users.noreply.github.com>
2018-08-20 10:34:25 +0100

    Add files via upload

---
+++ VOCpublisher.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 12:12:21 2018
+
+@author: chris
+"""
+
+import paho.mqtt.publish as publish
+
+MQTT_SERVER = "10.0.100.130"
+MQTT_PATH = "time"
+MQTT_PATH2 = "VOCs" 
+MQTT_PATH3 = "eCO2"
+MQTT_PATH4 = "Temp"
+
+import time
+from time import sleep
+from Adafruit_CCS811 import Adafruit_CCS811
+
+ccs =  Adafruit_CCS811()
+
+while not ccs.available():
+	pass
+temp = ccs.calculateTemperature()
+ccs.tempOffset = temp - 25.0
+
+try:
+    start_time = time.time()
+    while(1):
+        if ccs.available():
+            temp = str(ccs.calculateTemperature())
+            if not ccs.readData():
+                x = str(ccs.getTVOC()) 
+                y = str(ccs.geteCO2()) 
+                with open('VOCdatainitial.txt' ,'a+') as f:
+                    t = str(time.time() - start_time) 
+                    s = t + ' ' + x + ' ' + y + ' ' + temp
+                    print s
+                    print >>f, s
+                
+                msgs = [{'topic':MQTT_PATH2, 'payload':x},(MQTT_PATH3,y,0,False),(MQTT_PATH4,temp,0,False),(MQTT_PATH,t,0,False)]
+                publish.multiple(msgs,hostname=MQTT_SERVER)
+    
+            else:
+                publish.single("ERROR", hostname=MQTT_SERVER)
+                while(1):
+                    pass
+        sleep(5)
+    f.close() 
+     
+except:
+    print 'Time files when you\'re having fun'
+    while(1):
+        if ccs.available():
+            temp = str(ccs.calculateTemperature())
+            if not ccs.readData():
+                x = str(ccs.getTVOC()) 
+                y = str(ccs.geteCO2()) 
+                with open('VOCdatabackup.txt' ,'a+') as f:
+                    t = str(time.time() - start_time) 
+                    s = t + ' ' + x + ' ' + y + ' ' + temp
+                    print s
+                    print >>f, s
+    
+            else:
+                print 'no'
+                while(1):
+                    pass
+        sleep(5)
+    f.close()
+        
+            
+            
+            
+            
+
+ 
---
+++ VOCsubscriber.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 11:14:21 2018
+
+@author: chris
+"""
+
+#import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.subscribe as subscribe
+import matplotlib.pyplot as plt
+import pandas as pd
+    
+df = pd.DataFrame(columns=['Time','VOCs','eCO2','Temp'])
+with open('homedata.csv', 'w+') as f:
+        df.to_csv(f, header=True,index=False)
+        
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+
+def plotting(axes,x,y,colour,label,ylabel):
+    axes.clear()
+    axes.plot(x,y,color=colour,label=label)
+    axes.legend()
+    axes.grid()
+    axes.set_ylabel(ylabel)
+    
+       
+topics = ['#']
+i = 0
+N = 10000
+while i < N:
+    df_temp = pd.DataFrame(index=range(0,1), columns=['Time','VOCs','eCO2','Temp'])
+    m = subscribe.simple(topics, hostname="10.0.100.130", retained=False, msg_count=4)
+    for a in m:
+        y = a.payload
+        if a.topic == 'time':
+            df_temp['Time'][0] = float(y.decode())
+        if a.topic == 'VOCs':
+            df_temp['VOCs'][0] = float(y.decode())
+        if a.topic == 'eCO2':
+            df_temp['eCO2'][0] = float(y.decode())
+        if a.topic == 'Temp':
+            df_temp['Temp'][0] = float(y.decode())
+    print('message count:',i)
+    df = df.append(df_temp)
+    with open('homedata.csv', 'a+') as f:
+        df_temp.to_csv(f, header=False)
+        
+    if i < 100:
+        plotting(ax1,df['Time'], df['VOCs'],colour='red',label='VOCs',ylabel='ppb')
+        plotting(ax2,df['Time'],df['eCO2'],colour='green',label='eCO2',ylabel='ppm')
+        plotting(ax3,df['Time'],df['Temp'],colour='blue',label='Chip Temperature',ylabel='$^\circ$C')    
+        ax3.set_xlabel('Time (s)')
+        plt.pause(0.01)
+        plt.show()
+    else:
+        plotting(ax1,df['Time'][i-100:i], df['VOCs'][i-100:i],colour='red',label='VOCs',ylabel='ppb')
+        plotting(ax2,df['Time'][i-100:i],df['eCO2'][i-100:i],colour='green',label='eCO2',ylabel='ppm')
+        plotting(ax3,df['Time'][i-100:i],df['Temp'][i-100:i],colour='blue',label='Chip Temperature',ylabel='$^\circ$C')    
+        ax3.set_xlabel('Time (s)')
+        plt.pause(0.01)
+        plt.show()
+    i += 1
+    
+print(df)
+    
+    
