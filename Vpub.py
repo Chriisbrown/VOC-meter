e9e64208e1fbbc3116c9431752e3c74edb9ce245
Chriisbrown <41648127+Chriisbrown@users.noreply.github.com>
2018-08-20 12:32:36 +0100

    Update Vpub.py

--- Vpub.py
+++ Vpub.py
@@ -17,6 +17,21 @@ import time
 from time import sleep
 from Adafruit_CCS811 import Adafruit_CCS811
 
+def inputno(g, a, lim1, lim2):
+#This defines the function used to check user inputs are within range and of correct data type
+    if g == 1:
+        while True:
+            try:
+                h = int(input(a))
+                if lim1 <= h <= lim2:
+                    return h
+                else:
+                    time.sleep(0.033)
+            except ValueError:
+                time.sleep(0.033)
+                continue
+#If a wrong value is entered the Red LED will flash
+
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
@@ -24,50 +39,60 @@ while not ccs.available():
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
 
-try:
-    start_time = time.time()
-    while(1):
-        if ccs.available():
-            temp = str(ccs.calculateTemperature())
-            if not ccs.readData():
-                x = str(ccs.getTVOC()) 
-                y = str(ccs.geteCO2()) 
-                with open('VOCdatainitial.txt' ,'a+') as f:
-                    t = str(time.time() - start_time) 
-                    s = t + ' ' + x + ' ' + y + ' ' + temp
-                    print s
-                    print >>f, s
-                
-                msgs = [{'topic':MQTT_PATH2, 'payload':x},(MQTT_PATH3,y,0,False),(MQTT_PATH4,temp,0,False),(MQTT_PATH,t,0,False)]
-                publish.multiple(msgs,hostname=MQTT_SERVER)
-    
-            else:
-                publish.single("ERROR", hostname=MQTT_SERVER)
-                while(1):
-                    pass
-        sleep(5)
-    f.close() 
-     
-except:
-    print 'Time files when you\'re having fun'
-    while(1):
-        if ccs.available():
-            temp = str(ccs.calculateTemperature())
-            if not ccs.readData():
-                x = str(ccs.getTVOC()) 
-                y = str(ccs.geteCO2()) 
-                with open('VOCdatabackup.txt' ,'a+') as f:
-                    t = str(time.time() - start_time) 
-                    s = t + ' ' + x + ' ' + y + ' ' + temp
-                    print s
-                    print >>f, s
-    
-            else:
-                print 'no'
-                while(1):
-                    pass
-        sleep(5)
-    f.close()
+Menu = '0'
+while Menu != 'q':
+    Menu = input('Press 1 to begin log, press q to quit: ')
+#Menu setup allowing the program to be run multiple times without turning the pi off
+    now = datetime.datetime.now()
+    if Menu == '1':
+        frequency = inputno(1,'Frequency of data reading: ',1,100)
+        length = int((inputno(1,'Run test for how many minutes: ',1,1500))*60/frequency)
+#All relevant parameters, with incorrect user input checking
+        i = 0
+        start_time = time.time()
+	try:
+	    while(1):
+		if ccs.available():
+		    temp = str(ccs.calculateTemperature())
+		    if not ccs.readData():
+			x = str(ccs.getTVOC()) 
+			y = str(ccs.geteCO2()) 
+			with open('VOCdatainitial'+now.strftime("%Y-%m-%d %H-%M")+'.txt' ,'a+') as f:
+			    t = str(time.time() - start_time) 
+			    s = t + ' ' + x + ' ' + y + ' ' + temp
+			    print s
+			    print >>f, s
+
+			msgs = [{'topic':MQTT_PATH2, 'payload':x},(MQTT_PATH3,y,0,False),(MQTT_PATH4,temp,0,False),(MQTT_PATH,t,0,False)]
+			publish.multiple(msgs,hostname=MQTT_SERVER)
+
+		    else:
+			publish.single("ERROR", hostname=MQTT_SERVER)
+			while(1):
+			    pass
+		sleep(5)
+	    f.close() 
+
+	except:
+	    print 'Time files when you\'re having fun'
+	    while(1):
+		if ccs.available():
+		    temp = str(ccs.calculateTemperature())
+		    if not ccs.readData():
+			x = str(ccs.getTVOC()) 
+			y = str(ccs.geteCO2()) 
+			with open('VOCdatabackup'+now.strftime("%Y-%m-%d %H-%M")+'.txt'' ,'a+') as f:
+			    t = str(time.time() - start_time) 
+			    s = t + ' ' + x + ' ' + y + ' ' + temp
+			    print s
+			    print >>f, s
+
+		    else:
+			print 'no'
+			while(1):
+			    pass
+		sleep(5)
+	    f.close()
         
             
             
